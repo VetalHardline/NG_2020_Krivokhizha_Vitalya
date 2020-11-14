4a43a54ae79787c2491dbe4050cf7045261d8195
Vitalya <vvvetal2003@gmail^Com>
2020-11-14 23:13:44 +0200

    Task 4 changed

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -8,21 +8,26 @@ int main()
     cout << "Privet drug! Skolko poluchaesh deneg?" << endl;
     cin >> a;
 
-    if (a < 1000 && a >= 0) {
+    if (a < 1000) {
+            if(a > 0) {
         cout << "Rabotai bolshe";
     }
-
-    if(a >= 1000 && a < 1000000) {
-        cout << "Tak derzhat!";
     }
 
-    if(a >= 1000000) {
+    if(a > 1000) {
+        if(a < 1000000) {
+        cout << "Tak derzhat!";
+    }
+    }
+
+    if(a > 1000000) {
         cout << "Ti millioner!";
     }
 
     if(a < 0) {
         cout << "Ti menya obmanivaesh :)";
     }
+
     cout << ", no ti molodec!";
     return 0;
 }
