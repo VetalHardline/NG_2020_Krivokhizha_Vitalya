ba05c340a6b078f412fbc667bb4d12072d18ab55
Vitalya <vvvetal2003@gmail^Com>
2020-11-15 16:58:17 +0200

    Task 7 complete

---
+++ Lesson_1/Task_7/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int b;
+    cout << "VVedite chislo: ";
+cin >> b;
+
+    for(int a=0; a<=b; a++){
+    cout << a <<';';
+}
+
+    return 0;
+}
