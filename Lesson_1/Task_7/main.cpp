ee2bd7c1e4c593c113f2f65eca4e11ad136cb471
Vitalya <vvvetal2003@gmail^Com>
2020-11-17 20:10:53 +0200

    Task 7 changed

--- Lesson_1/Task_7/main.cpp
+++ Lesson_1/Task_7/main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main()
 {
-    int b;
+    int a, b;
     cout << "VVedite chislo: ";
     cin >> b;
 
-    for (int a=0; a<=b; a++)
+    for (a=0; a < b; a++)
     {
-    cout << a <<';';
+    cout << a <<',';
     }
+    cout << a;
     return 0;
 }
