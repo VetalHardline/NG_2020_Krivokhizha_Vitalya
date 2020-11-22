c21e2184497fcc8833d274fce8ccbdf60e7c0f3e
Vitalya <vvvetal2003@gmail^Com>
2020-11-22 15:30:18 +0200

    Task 6 changed

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -8,42 +8,42 @@ int main()
     cin >> objectSize;
 
     for (int j = 0; j < objectSize; j++) {
-    for (int i = 0; i < objectSize; i++)
-    cout << "*";
-    cout << endl;
+        for (int i = 0; i < objectSize; i++)
+            cout << "*";
+        cout << endl;
     }
 
     cout << endl;
 
     for (int j = 0; j < objectSize; j++) {
-    for (int i = 0; i < j+1; i++)
-    cout << "*";
-    cout << endl;
+        for (int i = 0; i < j+1; i++)
+            cout << "*";
+        cout << endl;
     }
 
     cout << endl;
 
     for (int j = 0; j < objectSize; j++) {
-    for (int i = 0; i < objectSize - j; i++)
-    cout << "*";
-    cout << endl;
+        for (int i = 0; i < objectSize - j; i++)
+            cout << "*";
+        cout << endl;
     }
 
     cout << endl;
 
     for (int j = 0; j < objectSize; j++) {
-    for (int i = 0; i < objectSize - j; i++) cout << " ";
-    for (int i = 0; i < j+1; i++)
-    cout << "*";
-    cout << endl;
+        for (int i = 0; i < objectSize - j; i++) cout << " ";
+        for (int i = 0; i < j+1; i++)
+            cout << "*";
+        cout << endl;
     }
 
     cout << endl;
 
     for (int j = 0; j < objectSize; j++) {
-    for (int i = 0; i < objectSize; i++) cout << " ";
-    for (int i = 0; i < j+1; i++)
-    cout << "*";
-    cout << endl;
+        for (int i = 0; i < objectSize; i++) cout << " ";
+        for (int i = 0; i < j+1; i++)
+            cout << "*";
+        cout << endl;
     }
 }
