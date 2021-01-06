fa7f559c2b0dabf84f0e79e0081cabdf946f6d8f
Vitalya <vvvetal2003@gmail^Com>
2021-01-06 16:05:27 +0200

    Task 3 changed

--- Lesson_2/Task_3/main.cpp
+++ Lesson_2/Task_3/main.cpp
@@ -4,21 +4,32 @@ using namespace std;
 
 int main()
 {
-    const int Size = 20;
+     const int Size = 20;
     int arr[Size];
     int maxSize = 0;
-    int i = 0;
+    int shos;
+    int i=0;
 
-    while(i < Size && arr[i]!=0)
+    do
     {
         cout << "Enter number: ";
         cin >> arr[i];
-        cout << arr[i];
+        if(arr[i] > maxSize)
+        maxSize = arr[i];
         i++;
+    } while(i < Size && arr[i-1]!=0);
+
+    for(i = 0; i < 20; i++){
+        for(shos = 0; shos < (maxSize - arr[i])/2; shos++){
+            cout << " ";
+        }
+        for(int q=0; q<arr[i]; q++){
+            cout << "*";
+        }
+        cout << endl;
+        if(arr[i] == 0) break;
     }
-
-
-
-
+    cout << endl;
 
 }
+
