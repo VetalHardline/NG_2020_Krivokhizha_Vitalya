9bdab42530860f3191e12325f0653a11767d0545
Vitalya <vvvetal2003@gmail^Com>
2021-01-08 16:41:46 +0200

    Task 1 complete

--- Lesson_4/Task_1/main.cpp
+++ Lesson_4/Task_1/main.cpp
@@ -2,44 +2,66 @@
 
 using namespace std;
 
-/*void vivesticarti(int card[])
+
+int allMoney = 0;
+
+void vivesticarti(int card[])
 {
-   for (int j = 0; j < 10; j++)
+    for (int j = 0; j < 10; j++)
     {
         cout << card[j] << " ";
     }
-}*/
+    cout << endl;
+}
 
-int main()
+int getUserSize()
 {
-    int Size=10;
-    int card[10]{}, money, allMoney;
-    money = 0;
-    allMoney = 0;
+    int Size;
+    cout << "Vvedite nomer kartochki: ";
+    cin >> Size;
+    return Size;
+}
 
-    while (true)
+int getUserMoney()
+{
+    int money;
+    cout << "skolko polozhit deneg? ";
+    cin >> money;
+    return money;
+}
+
+int suma(int money)
+{
+
+    cout << "v summe na vsex kartochkah - ";
+    allMoney = allMoney + money;
+    cout << allMoney;
+    cout << endl;
+    return allMoney;
+}
+
+void cikl()
+{
+    int card[10]{};
+    while(true)
     {
-        cout << "Vvedite nomer kartochki: ";
-        cin >> Size;
-        if (Size > 0 && Size < 11){
-            cout << "skolko polozhit deneg? ";
-            cin >> money;
+        int Size = getUserSize();
+
+        if (Size > 0 && Size < 11)
+        {
+            int money = getUserMoney();
             card[Size - 1] += money;
-
-            for (int j = 0; j < 10; j++)
-            {
-                cout << card[j] << " ";
-            }
-
-            cout << endl;
-            cout << "v summe na vsex kartochkah - ";
-            allMoney = allMoney + money;
-            cout << allMoney;
-            cout << endl;
+            vivesticarti(card);
+            suma(money);
         }
         else{
             cout << "takoi kartochki ne znayu" << endl;
         }
+
     }
-   // vivesticarti(card);
+}
+
+int main()
+{
+    cikl();
 }
