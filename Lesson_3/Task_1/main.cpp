894d0111d45b26c6323258a169a0d8323e724303
Vitalya <vvvetal2003@gmail^Com>
2021-01-30 13:48:07 +0200

    Task 1 changed

--- Lesson_3/Task_1/main.cpp
+++ Lesson_3/Task_1/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main()
 {
     char stroka[255];
-    int kolichestvoslov = 1;
+    int kolichestvoslov = 0;
 
     cout << "Enter string: ";
     cin.getline(stroka, 255);
 
-    for(int elem = 0; stroka[elem + 1] != '\0'; elem++)
+    for(int elem = 0; stroka[elem] != '\0'; elem++)
     {
         if((stroka[elem] >= 'a' && stroka[elem] <= 'z') || (stroka[elem] >= 'A' && stroka[elem] <= 'Z')){
             if(!(stroka[elem + 1] >= 'a' && stroka[elem + 1] <= 'z') && !(stroka[elem + 1] >= 'A' && stroka[elem + 1] <= 'Z')){
