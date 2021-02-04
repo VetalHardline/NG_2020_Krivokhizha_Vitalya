0b93fcb44315dd2d69b88af382158b4924b224d8
Vitalya <vvvetal2003@gmail^Com>
2021-02-04 18:03:27 +0200

    test commit with using smiley 📄 update copyright year

---
+++ Lesson_6/Task_3/kalkulator.cpp
@@ -0,0 +1,25 @@
+#include "kalkulator.h"
+#include "ui_kalkulator.h"
+
+kalkulator::kalkulator(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::kalkulator)
+{
+    ui->setupUi(this);
+
+    connect(ui->suma, &QPushButton::clicked, this, &kalkulator::resultatNazhatia);
+    ui->resultat->setReadOnly(true);
+}
+
+kalkulator::~kalkulator()
+{
+    delete ui;
+}
+
+void kalkulator::resultatNazhatia()
+{
+    int userNumber1, userNumber2;
+    userNumber1 = ui->chislo1->value();
+    userNumber2 = ui->chislo2->value();
+    ui->resultat->setValue(userNumber1 + userNumber2);
+}
